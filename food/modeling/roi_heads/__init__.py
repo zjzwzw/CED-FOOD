8515a510d14c6069fb81c28d0517787a3591503c
zjzwzw <1638098545@qq.com>
2024-06-28 15:22:33 +0800

    fix bug

--- food/modeling/roi_heads/__init__.py
+++ food/modeling/roi_heads/__init__.py
@@ -1,6 +1,6 @@
 from .box_head import ROI_BOX_HEAD_REGISTRY, build_box_head
 from .roi_heads import (
-    ROI_HEADS_REGISTRY, ROIHeads, StandardROIHeads, build_roi_heads, select_foreground_proposals, OpenSetStandardROIHeads)
+    ROI_HEADS_REGISTRY, ROIHeads, StandardROIHeads, build_roi_heads, select_foreground_proposals)
 
 from .clip_roi_heads import (
     CLIPRes5ROIHeads,
